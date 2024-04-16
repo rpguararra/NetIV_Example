e1a6915da15ff45ce3b43b8a38040c621333ff36
rguararra <31391018+rguararra@users.noreply.github.com>
2024-04-16 01:05:37 -0700

    Added code for api and added entity framework to the project.

---
+++ WebAPI_1/DB.cs
@@ -0,0 +1,21 @@
+﻿namespace WebAPI_1
+{
+    using Microsoft.EntityFrameworkCore;
+
+    public class DB : DbContext
+    {
+
+        public DB(string conStr) 
+        {
+            this.Database.SetConnectionString(conStr);
+        }
+
+        public DB(DbContextOptions<DB> options) : base(options)
+        {
+        }
+
+        public DbSet<NoticeModel> NoticeModels => Set<NoticeModel>();
+        public DbSet<ManagerModel> ManagerModels => Set<ManagerModel>();
+
+    }
+}
---
+++ WebAPI_1/ManagerModel.cs
@@ -0,0 +1,12 @@
+﻿namespace WebAPI_1
+{
+    public class ManagerModel
+    {
+        public string? id { get; set; }
+        public string? phone { get; set; }
+        public string? jurisdiction { get; set; }
+        public string? identificationNumber { get; set; }
+        public string? firstName { get; set; }
+        public string? lastName { get; set; }
+    }
+}
